673f29adf942e80309bee3865c2690bcd83299d3
dawidbeno <dawid.beno@gmail.com>
2017-05-04 11:06:16 +0200

    extended GUI

--- python_module/guiMain.py
+++ python_module/guiMain.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt4 import QtGui
 
-from src.form import Ui_Form
 from src import controller
+from src.form import Ui_Form
 
 app = QtGui.QApplication(sys.argv)
 Form = QtGui.QWidget()
@@ -15,3 +15,36 @@ controller.init(ui)
 Form.show()
 
 sys.exit(app.exec_())
+
+
+# class MainGuiClass(QtGui.QMainWindow, form.Ui_Form):
+#     def __init__(self, parent = None):
+#         super(MainGuiClass, self).__init__(parent)
+#         self.setupUi(self)
+#         self.threadClass = ThreadClass()
+#         self.threadClass.start()
+#
+#
+#
+#
+#
+# class ThreadClass(QtCore.QThread):
+#     def __init__(self, parent = None):
+#         super(ThreadClass, self).__init__(parent)
+#
+#     def conInit(self, ui):
+#         controller.init(ui)
+#
+#
+# if __name__ == '__main__':
+#     a = QtGui.QApplication(sys.argv)
+#     app = MainGuiClass()
+#     app.show()
+#     a.exec_()
+#
+
+
+
+
+
+
--- python_module/src/controller.py
+++ python_module/src/controller.py
@@ -27,19 +27,37 @@ sucReallocs = 0
 
 bestAllocTime = 9999
 worstAllocTime = 0
+countAllocTime = 0
 
 bestFreeTime = 9999
 worstFreeTime = 0
+countFreeTime = 0
 
 bestReallocTime = 9999
 worstReallocTime = 0
+countReallocTime = 0
 
 maxAllocBlock = 0
 minAllocBlock = 5000
 sizeCount = 0
-avarageAllocBlock = 5000
+averageAllocBlock = 5000
+numOfExtFragFail = 0
+
+maxFreeBlock = 0
+minFreeBlock = 5000
+averageFreeBlock = 5000
+numOfUnusable = 0
+numOfFreeBlocks = 0
 
 wholeTime = 0
+
+stepNum = 0
+numOfSteps = 0
+
+numOfBlocks = 3
+
+internalFragment = 0
+
 
 # Arduino uses BEST fit as default algorithm
 
@@ -70,6 +88,7 @@ def init(ui):
                 print "waiting for init ..."
     except Exception:
         print "Serial port not found"
+        ui.appnedST("FATAL ERROR: Serial port not found. Communication will not work")
 
 
 
@@ -79,8 +98,12 @@ def setAlgorithm(ui, algType):
     global sucAllocs, sucFrees, sucReallocs
     global bestFreeTime, bestReallocTime, bestAllocTime
     global worstFreeTime, worstReallocTime, worstAllocTime
-    global maxAllocBlock, minAllocBlock, avarageAllocBlock, sizeCount
-    global wholeTime
+    global countAllocTime, countFreeTime, countReallocTime
+    global maxAllocBlock, minAllocBlock, averageAllocBlock, sizeCount
+    global wholeTime, stepNum, numOfSteps
+    global internalFragment, numOfExtFragFail
+    global numOfBlocks
+    global maxFreeBlock, minFreeBlock, averageFreeBlock, numOfUnusable, numOfFreeBlocks
     numOfAllocs = 0
     numOfFrees = 0
     numOfReallocs = 0
@@ -89,18 +112,32 @@ def setAlgorithm(ui, algType):
     sucReallocs = 0
     bestAllocTime = 9999
     worstAllocTime = 0
+    countAllocTime =0
     bestFreeTime = 9999
     worstFreeTime = 0
+    countFreeTime = 0
     bestReallocTime = 99999
     worstReallocTime = 0
+    countReallocTime = 0
     maxAllocBlock = 0
     minAllocBlock = 9999
-    avarageAllocBlock = 9999
+    averageAllocBlock = 9999
     sizeCount = 0
     wholeTime = 0
+    stepNum = 0
+    numOfExtFragFail = 0
+    internalFragment = 0
+    maxFreeBlock = 0
+    minFreeBlock = 5000
+    averageFreeBlock = 5000
+    numOfUnusable = 0
+    numOfFreeBlocks = 0
+    numOfStep = 0
+    numOfBlocks = 3
     ui.setST("")
     ui.setAT("")
     ui.setES("")
+    ui.setPBT(0)
     if(algType == BEST):
         serialComm.write(b'b')
         while True:
@@ -150,9 +187,12 @@ def setAlgorithm(ui, algType):
 # ***************************************
 
 def load_1(ui):
-    global TEST_DIR, ADDRS
+    global TEST_DIR, ADDRS, stepNum, numOfSteps
+    stepNum = 0
     try:
         file = open(TEST_DIR+"level_1", "r").read()
+        numOfSteps = sum(1 for line in open(TEST_DIR+"level_1"))
+        numOfSteps -= 2
     except Exception:
             print "File not found"
             return
@@ -167,9 +207,12 @@ def load_1(ui):
     return file
 
 def load_2(ui):
-    global TEST_DIR, ADDRS
+    global TEST_DIR, ADDRS, stepNum, numOfSteps
+    stepNum = 0
     try:
         file = open(TEST_DIR+"level_2", "r").read()
+        numOfSteps = sum(1 for line in open(TEST_DIR + "level_2"))
+        numOfSteps -= 2
     except Exception:
             print "File not found"
             return
@@ -184,9 +227,12 @@ def load_2(ui):
     return file
 
 def load_3(ui):
-    global TEST_DIR, ADDRS
+    global TEST_DIR, ADDRS, stepNum, numOfSteps
+    stepNum = 0
     try:
         file = open(TEST_DIR+"level_3", "r").read()
+        numOfSteps = sum(1 for line in open(TEST_DIR + "level_3"))
+        numOfSteps -= 2
     except Exception:
             print "File not found"
             return
@@ -201,9 +247,12 @@ def load_3(ui):
     return file
 
 def load_4(ui):
-    global TEST_DIR, ADDRS
+    global TEST_DIR, ADDRS, stepNum, numOfSteps
+    stepNum = 0
     try:
         file = open(TEST_DIR+"level_4", "r").read()
+        numOfSteps = sum(1 for line in open(TEST_DIR + "level_4"))
+        numOfSteps -= 2
     except Exception:
             print "File not found"
             return
@@ -219,15 +268,15 @@ def load_4(ui):
 
 
 def nextStep(ui, step):
+    global stepNum, numOfSteps
+    stepNum += 1
+    p = ((float(stepNum) / float(numOfSteps)) *100)
+    ui.setPBT(p)
     ui.appendST("NEXT STEP")
     runStep(ui, step)
     time.sleep(2)
     printMemory(ui)
     ui.appendES(step)
-
-
-
-
 
 
 # *********************************************
@@ -253,9 +302,10 @@ def runStep(ui, step):
 
 def allocateMem(ui, step):
     global serialComm
-    global bestAllocTime, worstAllocTime, sucAllocs
-    global maxAllocBlock, minAllocBlock, avarageAllocBlock, sizeCount
-    print "\n **** Start test allocation ****"
+    global bestAllocTime, worstAllocTime, sucAllocs, countAllocTime
+    global maxAllocBlock, minAllocBlock, averageAllocBlock, sizeCount
+    global stepNum, numOfExtFragFail
+    print "\n **** Start test allocation **** StepNum:"+str(stepNum)
     ui.appendST("Request type:                   Allocation\n")
     size = step.split(' ')[1]
     size = str(size)
@@ -280,6 +330,7 @@ def allocateMem(ui, step):
                 addr = line.split(':')[2]
                 duration = line.split(':')[3]
                 duration = duration[:-2]
+                countAllocTime += int(duration)
                 i = 0
                 while True:
                     if (ADDRS[i] == None):
@@ -301,7 +352,7 @@ def allocateMem(ui, step):
                 if(int(size) > maxAllocBlock):
                     maxAllocBlock = int(size)
                 sizeCount += int(size)
-                avarageAllocBlock = (sizeCount / numOfAllocs)
+                averageAllocBlock = (sizeCount / numOfAllocs)
             elif(line.__contains__("FAILED")):
                 ui.appendST("Success rate of request:  Failed\n")
                 remMem = line.split(':')[1]
@@ -310,6 +361,7 @@ def allocateMem(ui, step):
                     ui.appendST("Fail has been caused due to external fragmentation:")
                     ui.appendST("Requested size: " + str(size) + " B\nRemaining memory: "+ str(remMem)+" B")
                     ui.appendST("\nMemory is divided into small blocks\n")
+                    numOfExtFragFail += 1
             break
         print line
         time.sleep(0.5)
@@ -317,13 +369,17 @@ def allocateMem(ui, step):
 
 def freeMem(ui, step):
     global serialComm
-    global bestFreeTime, worstFreeTime, sucFrees
+    global bestFreeTime, worstFreeTime, sucFrees, countFreeTime
+    global stepNum
     numOfJoins = 0
-    print "Start test free"
+    print "\n -------  Start test FREE  --------- StepNum:"+str(stepNum)
     ui.appendST("Request type:                   Free\n")
     ptr = step.split(' ')[1]
     ptr = ptr[3:]
     ptr = str(ptr)
+    if(ADDRS[int(ptr)] == None):
+        print "ERROR(Free): index doesnt exists"
+        return
     serialComm.write(b'f')
     if (len(ptr) == 1):
         serialComm.write(b'1')
@@ -340,6 +396,7 @@ def freeMem(ui, step):
             print line
             sucFrees += 1
             duration = line.split(':')[2]
+            countFreeTime += int(duration)
             numOfJoins = line.split(':')[3]
             numOfJoins = numOfJoins[:-2]
             ui.appendST("Success rate of request:   Successful\n")
@@ -351,21 +408,31 @@ def freeMem(ui, step):
             if (int(duration) >= worstFreeTime):
                 worstFreeTime = int(duration)
             break
+        elif(line.__contains__("FAILED")):
+            print "Fail free> "+line
+            ui.setST("")
+            ui.appendST("Success rate of request:   Failed\n")
+            ui.appendST("Request satisfying time: 0ms")
+            ui.appendST("Number of block joins: 0")
+            break
         print line
         time.sleep(1)
 
 
 def reallocMem(ui, step):
     global serialComm
-    global bestReallocTime, worstReallocTime, sucReallocs
-    print "Start test realloc"
+    global bestReallocTime, worstReallocTime, sucReallocs, countReallocTime
+    global stepNum
+    print "\n +++++++++++  Start test realloc +++++++++++ StepNum:"+str(stepNum)
     ui.appendST("Request type:                   Reallocation\n")
     ptr = step.split(' ')[1]
     ptr = ptr[3:]
     ptr = str(ptr)
     size = step.split(' ')[2]
     size = str(size)
-
+    if (ADDRS[int(ptr)] == None):
+        print "ERROR(realloc): index doesnt exists"
+        return
     serialComm.write(b'r')
     if (len(ptr) == 1):
         serialComm.write(b'1')
@@ -392,6 +459,7 @@ def reallocMem(ui, step):
             print line
             sucReallocs += 1
             duration = line.split(':')[1]
+            countReallocTime += int(duration)
             addr = line.split(':')[2]
             oldSize = line.split(':')[3]
             oldSize = oldSize[:-2]
@@ -404,6 +472,15 @@ def reallocMem(ui, step):
                 bestReallocTime = int(duration)
             if (int(duration) >= worstReallocTime):
                 worstReallocTime = int(duration)
+            break
+        elif (line.__contains__("FAILED")):
+            print line
+            ui.setST("")
+            ui.appendST("Success rate of request:   Failed\n")
+            ui.appendST("Old size of block:      0B")
+            ui.appendST("New size of block:      0B")
+            ui.appendST("Request satisfying time: 0ms")
+            ui.appendST("Beginning address of new block: 0")
             break
         print line
         time.sleep(1)
@@ -419,15 +496,41 @@ def printMemory(ui):
     global serialComm
     global remainingMem
     global wholeTime
+    global numOfFreeBlocks, minFreeBlock, maxFreeBlock, averageFreeBlock
+    global numOfUnusable, numOfBlocks
+    global minFreeBlock, maxFreeBlock
     print "ACTUAL MEMORY STATE from python PrintMemory"
     ui.setMV("")
     serialComm.write(b'p')
+    numOfFreeBlocks = 0
+    numOfBlocks = 0
+    numOfUnusable = 0
+    minFreeBlock = 5000
+    maxFreeBlock = 0
+
+    size = 0
+    count = 0
     while True:
         line = serialComm.readline()
         if (line.__contains__("Finish")):
             break
         if(not (line.__contains__("Counter") or line.__contains__("WholeTime"))):
+            if(line.__contains__("FREE")):
+                var = line.split(':')[2]
+                var = var[:-6]
+                var = int(var)
+                numOfFreeBlocks += 1
+                if(int(var) < int(minFreeBlock)):
+                    minFreeBlock = var
+                if(int(var) > int(maxFreeBlock)):
+                    maxFreeBlock = var
+                if(int(var) <= MEM_BLOCK_SIZE):
+                    numOfUnusable += 1
+                size += int(var)
+                count += 1
+                averageFreeBlock = (size / count)
             ui.appendMV(line)
+            numOfBlocks += 1
         if(line.__contains__("Remaining")):
             remainingMem = line.split(':')[1]
             remainingMem = remainingMem[:-2]
@@ -447,7 +550,10 @@ def showAllStats(ui):
     global sucAllocs, sucFrees, sucReallocs
     global bestFreeTime, bestReallocTime, bestAllocTime
     global worstFreeTime, worstReallocTime, worstAllocTime
-    global maxAllocBlock, minAllocBlock, avarageAllocBlock
+    global countAllocTime, countFreeTime, countReallocTime
+    global maxAllocBlock, minAllocBlock, averageAllocBlock
+    global numOfFreeBlocks, maxFreeBlock, minFreeBlock, averageFreeBlock, numOfUnusable
+    global numOfExtFragFail, numOfBlocks
     ui.setST("")
     if(int(numOfAllocs) == 0 and int(numOfFrees) == 0 and int(numOfReallocs) == 0):
         ui.appendST("No statistics available")
@@ -455,19 +561,29 @@ def showAllStats(ui):
     if(int(numOfAllocs) > 0):
         ui.appendST("Best alloc time: "+str(bestAllocTime)+"ms")
         ui.appendST("Worst alloc time: "+str(worstAllocTime)+"ms")
-        ui.appendST("Jitter: "+str(worstAllocTime - bestAllocTime)+"ms")
+        ui.appendST("Jitter: " + str(worstAllocTime - bestAllocTime) +"ms  Average: " + str(countAllocTime / sucAllocs) + "ms")
         ui.appendST("")
     if(int(numOfFrees) > 0):
         ui.appendST("Best free time: " + str(bestFreeTime)+"ms")
         ui.appendST("Worst free time: " + str(worstFreeTime)+"ms")
-        ui.appendST("Jitter: "+str(worstFreeTime - bestFreeTime)+"ms")
+        ui.appendST("Jitter: " + str(worstFreeTime - bestFreeTime) +"ms  Average: " + str(countFreeTime / sucFrees) + "ms")
         ui.appendST("")
     if(int(numOfReallocs) > 0):
         ui.appendST("Best realloc time: " + str(bestReallocTime)+"ms")
         ui.appendST("Worst realloc time: " + str(worstReallocTime)+"ms")
-        ui.appendST("Jitter: "+str(worstReallocTime - bestReallocTime)+"ms")
+        ui.appendST("Jitter: " + str(worstReallocTime - bestReallocTime) +"ms   Average: " + str(countReallocTime / sucReallocs) + "ms")
         ui.appendST("")
     ui.appendST("Time count of whole test: "+str(wholeTime)+"ms")
+    ui.appendST("")
+    ui.appendST("External fragmentation:")
+    ui.appendST("Largest free blok: "+str(maxFreeBlock)+"B")
+    ui.appendST("Smallet free block: "+str(minFreeBlock)+"B")
+    ui.appendST("Average free block: "+str(averageFreeBlock)+"B")
+    ui.appendST("Number of free blocks: "+str(numOfFreeBlocks))
+    ui.appendST("Number of unusable blocks: "+str(numOfUnusable))
+    ui.appendST("")
+    intFrag = round((float((numOfBlocks-2)*MEM_BLOCK_SIZE) / float(wholeMem)) * 100)
+    ui.appendST("Internal fragmentation: "+str((numOfBlocks-2)*MEM_BLOCK_SIZE)+"B ("+str(intFrag)+"%)")
     ui.appendST("")
     ui.appendST("Type and number of requests: ")
     ui.appendST("(all / success / failed / percentage of success)")
@@ -480,43 +596,17 @@ def showAllStats(ui):
     if (int(numOfReallocs) > 0):
         reallocPercentage = str(round((float(sucReallocs) / float(numOfReallocs)) * 100,3))
         ui.appendST("Reallocs: "+str(numOfReallocs)+" / "+str(sucReallocs)+" / "+str(int(numOfReallocs) - int(sucReallocs))+" /"+reallocPercentage+"%")
-    ui.appendST("\n")
+    ui.appendST("")
     ui.appendST("Memory utilization:")
     ui.appendST("Whole memory:  "+str(wholeMem)+"B")
-
     allocMemPercentage = str(round(((float(int(wholeMem) - int(remainingMem)) / float(wholeMem)) * 100),3))
     ui.appendST("Allocated memory:  "+str(int(wholeMem) - int(remainingMem))+"B  ("+str(allocMemPercentage)+" %)")
-
     freeMemPercentage = str(round(((float(remainingMem) / float(wholeMem)) * 100),3))
     ui.appendST("Free memory:  "+str(remainingMem)+"B  ("+str(freeMemPercentage)+" %)")
-
     ui.appendST("")
-    ui.appendST("Largest block: "+str(maxAllocBlock)+"B")
-    ui.appendST("Smallest block: "+str(minAllocBlock)+"B")
-    ui.appendST("Average block: "+str(avarageAllocBlock)+"B")
+    #ui.appendST("Largest allocated block: "+str(maxAllocBlock)+"B")
+    #ui.appendST("Smallest allocated block: "+str(minAllocBlock)+"B")
+    #ui.appendST("Average allocatd block: " + str(averageAllocBlock) + "B")
+    ui.appendST("Number of allocation fails due to external fagmentation: "+str(numOfExtFragFail))
     ui.setPB(allocMemPercentage)
-
-
-# ****** private functions ******
-
-def sendPtrNum(ui, ptrNum):
-    global serialComm
-    serialComm.write(b'n')
-    if (len(ptrNum) == 1):
-        serialComm.write(b'1')
-    elif (len(ptrNum) == 2):
-        serialComm.write(b'2')
-    elif (len(ptrNum) == 3):
-        serialComm.write(b'3')
-    elif (len(ptrNum) == 4):
-        serialComm.write(b'4')
-    serialComm.write(ptrNum)
-    while True:
-        line = serialComm.readline()
-        if(line.__contains__("Test prepare finish")):
-            print line
-            ui.appendST("Test prepare finish")
-            break
-        print line
-        time.sleep(0.1)
 
